86de9bd83db65b0fe7c3769a807f436ef8f05715
Michaela DeToma <mdetoma@umass.edu>
2024-11-08 22:01:09 -0500

    keep compiler happy

--- frontend/my-app/src/components/HomePage.tsx
+++ frontend/my-app/src/components/HomePage.tsx
@@ -1,3 +1,12 @@
 import React, {useState} from "react";
 
+import {InputBox} from './InputBox';
+import {AgeDrop} from './AgeDrop';
+import {Menu} from './Menu';
+import {NameLogo} from './NameLogo';
+
+
+
+
+
 
