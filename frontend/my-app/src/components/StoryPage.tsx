6dcebb9e3b4fb7b9b8a5ff0f652db3612b4881bb
T0BlN <tglock@umass.edu>
2024-11-08 23:48:13 -0500

    Viewer component base

--- frontend/my-app/src/components/StoryPage.tsx
+++ frontend/my-app/src/components/StoryPage.tsx
@@ -2,41 +2,17 @@ import React from 'react';
 import { Story } from '../components/DataContext';
 import Viewer from './Viewer';
 
-// interface StoryPageProps {
-//  story: Story;
-// }
+interface StoryPageProps {
+ story: Story;
+}
 
-// const StoryPage: React.FC<StoryPageProps> = ({ story }) => {
-//     return (
-//         <div>
-//           <h1>{story.name}</h1>
-//           <Viewer story={story} />
-//         </div>
-//     );
-// };
-const StoryPage: React.FC = () => {
-    // Dummy data for testing
-    const dummyStory: Story = {
-      name: 'My Test Story',
-      images: [
-        'https://via.placeholder.com/300', // Placeholder image URLs
-        'https://via.placeholder.com/300',
-        'https://via.placeholder.com/300'
-      ],
-      pages: [
-        'This is the first page of the story.',
-        'This is the second page of the story.',
-        'This is the third page of the story.'
-      ],
-      audios: []
-    };
-  
+const StoryPage: React.FC<StoryPageProps> = ({ story }) => {
     return (
-      <div>
-        <h1>{dummyStory.name}</h1>
-        <Viewer story={dummyStory} />
-      </div>
+        <div>
+          <h1>{story.name}</h1>
+          <Viewer story={story} />
+        </div>
     );
-  };
+};
 
 export default StoryPage;
