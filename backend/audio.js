82754c1435b12c2588c9000bc133dd5f2c4167db
John Lehmann <john.d.lehmann@gmail.com>
2024-11-09 10:56:09 -0500

    audio file extensions provided in story output

--- backend/audio.js
+++ backend/audio.js
@@ -6,14 +6,12 @@ function textToSpeech(text, filename = 'output.mp3') {
     return new Promise((resolve, reject) => {
         // Define the directory path for the 'temp' folder
         const tempDir = path.join(__dirname, 'temp');
-        
-        // Check if the 'temp' directory exists, create it if not
-        if (!fs.existsSync(tempDir)) {
-            fs.mkdirSync(tempDir);
-        }
 
         // Define the full path to save the file in the 'temp' directory
         const filePath = path.join(tempDir, filename);
+
+        // Create all directories that do not exist
+        fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
         const gtts = new gTTS(text, 'en');
 
--- backend/stories.js
+++ backend/stories.js
@@ -5,30 +5,14 @@
 const OpenAI = require('openai');
 const openai = new OpenAI({apiKey: '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'});
 
-const default_story = [
-  {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-]
+const { textToSpeech } = require('./audio.js');
+const uuid = require('uuid');
 
-//load image urls from default_images.json
 const fs = require('fs');
-const image_urls = JSON.parse(fs.readFileSync('default_images.json', 'utf8'));
-for (let i = 0; i < default_story.length; i++) {
-  default_story[i].image = image_urls[i];
-}
+const path = require('path');
+// load image urls from default_images.json
+const image_urls = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'default_images.json')));
+
 
 const generateStoryText = async (prompt, level) => {
   if (level === undefined) {
@@ -81,6 +65,16 @@ const generateStoryText = async (prompt, level) => {
     }
   });
   const jsonContent = JSON.parse(gptResponse.choices[0].message.content);
+  jsonContent.uuid = uuid.v4();
+
+  // generate audio files for tts for title and then each page
+  textToSpeech(jsonContent.cover.title, `${jsonContent.uuid}/title.mp3`);
+  jsonContent.cover.audio = `${jsonContent.uuid}/title.mp3`;
+  for(let i = 0; i < jsonContent.story.length; i++) {
+    textToSpeech(jsonContent.story[i].text, `${jsonContent.uuid}/page${i}.mp3`);
+    jsonContent.story[i].audio = `${jsonContent.uuid}/page${i}.mp3`;
+  }
+
   // first do cover image
   jsonContent.cover.image = await getImage(jsonContent.cover.image_description);
   delete jsonContent.cover.image_description;
