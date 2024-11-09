981a959cd6a9c0d622fc7149127ac782d242da17
John Lehmann <john.d.lehmann@gmail.com>
2024-11-09 09:09:49 -0500

    made basic file server

---
+++ backend/file-server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/files/:filepath', (req, res) => {
+  const filePath = req.params.filepath;
+  const absolutePath = path.resolve(`temp/${filePath}`);
+
+  fs.stat(absolutePath, (err, stats) => {
+    if (err) {
+      return res.status(404).send('File not found');
+    }
+
+    if (stats.isFile()) {
+      res.sendFile(absolutePath);
+    } else if (stats.isDirectory()) {
+      fs.readdir(absolutePath, (err, files) => {
+        if (err) {
+          return res.status(500).send('Unable to read directory');
+        }
+        res.json(files);
+      });
+    } else {
+      res.status(400).send('Path is neither a file nor a directory');
+    }
+  });
+});
+
+module.exports = router;
